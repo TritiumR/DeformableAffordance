251632aaaf7605b44f20b0b4f4ceb6083b2626c7
TritiumR <2000013179@stu.pku.edu.cn>
2022-09-25 16:48:41 +0800

    Update get_preprocess.py

--- softgym/get_preprocess.py
+++ softgym/get_preprocess.py
@@ -7,8 +7,6 @@ import pickle
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', default='./data/')
-    parser.add_argument('--save_path', default='./')
-    parser.add_argument('--render_demo', action='store_true')
     args = parser.parse_args()
 
     dirs = os.listdir(args.path)
@@ -16,11 +14,8 @@ def main():
     mean = np.zeros(4)
     std = np.zeros(4)
 
-    i = 0
     for fname in dirs:
-        i += 1
-        if (i == 100):
-            break
+        print(fname)
         data = pickle.load(open(os.path.join(args.path, fname), 'rb'))
         obs = np.array(data['obs'])
         for d in range(4):
@@ -30,12 +25,8 @@ def main():
     mean /= len(dirs)
     std /= len(dirs)
 
-    mean = np.tile(mean, (5, 5, 1))
-    std = np.tile(std, (5, 5, 1))
-    print(mean)
-    for d in range(4):
-        print(mean[:, :, d].std())
-        print(std[:, :, d].std())
+    print("mean: ", mean)
+    print("std: ", std)
 
 if __name__ == '__main__':
     main()
