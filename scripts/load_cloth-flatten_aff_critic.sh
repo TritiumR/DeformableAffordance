f8d39b57b260a1061c80cc124e6e3eaefe30ff73
TritiumR <101703696+TritiumR@users.noreply.github.com>
2022-10-19 10:38:18 +0800

    load both

--- scripts/load_cloth-flatten_aff_critic.sh
+++ scripts/load_cloth-flatten_aff_critic.sh
@@ -3,16 +3,16 @@ CUDA_VISIBLE_DEVICES=0 python ./softgym/test.py \
   --task cloth-flatten \
   --agent aff_critic \
   --num_demos 8000 \
-  --step 1 \
+  --step 2 \
   --out_logits 1 \
-  --exp_name 1018-07-ten-online-10000-step-1 \
+  --exp_name 1019-02-eleven-both_online-4000-step-2 \
   --process_num 1 \
   --num_test 20 \
   --critic_depth 1 \
-  --load_critic_dir checkpoints/cloth-flatten-Aff_Critic-8000-1001-04-tryten-step-1/critic-ckpt-301000.h5 \
-  --load_critic_mean_std_dir checkpoints/cloth-flatten-Aff_Critic-8000-1001-04-tryten-step-1 \
-  --load_aff_dir checkpoints/cloth-flatten-Aff_Critic-50000-1010-08-test_online-step-2-0.8-step-1/attention-online-ckpt-10000.h5 \
-  --load_aff_mean_std_dir checkpoints/cloth-flatten-Aff_Critic-50000-1010-08-test_online-step-2-0.8-step-1 \
+  --load_critic_dir checkpoints/cloth-flatten-Aff_Critic-10000-1018-08-online-step-2-both-0.8-step-1/critic-online-ckpt-4000.h5 \
+  --load_critic_mean_std_dir checkpoints/cloth-flatten-Aff_Critic-10000-1018-08-online-step-2-both-0.8-step-1 \
+  --load_aff_dir checkpoints/cloth-flatten-Aff_Critic-10000-1018-08-online-step-2-both-0.8-step-1/attention-online-ckpt-4000.h5 \
+  --load_aff_mean_std_dir checkpoints/cloth-flatten-Aff_Critic-10000-1018-08-online-step-2-both-0.8-step-1 \
   --image_size 160 \
   --save_video_dir './test_video/' \
   --headless 1
--- scripts/load_cloth-flatten_all.sh
+++ scripts/load_cloth-flatten_all.sh
@@ -10,12 +10,12 @@ do
   --step 2 \
   --test_step 10 \
   --out_logits 1 \
-  --exp_name 1018-15-step-2-true_best-30:50 \
+  --exp_name 1019-03-step-2-both_online-4000 \
   --test_id "$i" \
-  --load_critic_dir checkpoints/cloth-flatten-Aff_Critic-9000-1011-16-tryten_eleven-online-2:1-step-2/critic-ckpt-300000.h5 \
-  --load_critic_mean_std_dir checkpoints/cloth-flatten-Aff_Critic-9000-1011-16-tryten_eleven-online-2:1-step-2 \
-  --load_aff_dir checkpoints/cloth-flatten-Aff_Critic-20000-1010-08-test_online-step-3-0.7-step-1/attention-online-ckpt-17000.h5 \
-  --load_aff_mean_std_dir checkpoints/cloth-flatten-Aff_Critic-20000-1010-08-test_online-step-3-0.7-step-1 \
+  --load_critic_dir checkpoints/cloth-flatten-Aff_Critic-10000-1018-08-online-step-2-both-0.8-step-1/critic-online-ckpt-4000.h5 \
+  --load_critic_mean_std_dir checkpoints/cloth-flatten-Aff_Critic-10000-1018-08-online-step-2-both-0.8-step-1 \
+  --load_aff_dir checkpoints/cloth-flatten-Aff_Critic-10000-1018-08-online-step-2-both-0.8-step-1/attention-online-ckpt-4000.h5 \
+  --load_aff_mean_std_dir checkpoints/cloth-flatten-Aff_Critic-10000-1018-08-online-step-2-both-0.8-step-1 \
   --image_size 160 \
   --set_flat 0 \
   --save_video_dir './test_video/' \
